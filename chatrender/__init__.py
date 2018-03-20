35bb1c71394aafa56d16de0791e8f3c64532bce7
Jon McClure <jmcclure@politico.com>
2018-03-20 17:02:53 -0400

    0.1.5

--- chatrender/__init__.py
+++ chatrender/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 __package__ = 'django-politico-slackchat-renderer'
 
 default_app_config = 'chatrender.apps.ChatrenderConfig'
--- chatrender/staticapp/src/js/main-election.jsx
+++ chatrender/staticapp/src/js/main-election.jsx
@@ -9,3 +9,9 @@ import '../scss/election/main.scss';
 import Chat from './election/chat.jsx';
 
 ReactDOM.render(<Chat />, document.getElementById('blog'));
+
+window.$('#ask-politico').click((e) => {
+  e.preventDefault();
+  const status = encodeURIComponent('#AskPOLITICO');
+  window.open(`https://twitter.com/intent/tweet?text=${status}`);
+});
